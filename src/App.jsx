39e2006deb746fc68bf3ca59ba8d7a4e49cc29b9
andrea ilieschi <andrea.ilieschi@gmail.com>
2024-06-01 13:09:42 +0200

    build routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
+
+import './server';
 
 export default function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link to="/">#VANLIFE</Link>
+        <Link className="site-logo" to="/">
+          #VANLIFE
+        </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
